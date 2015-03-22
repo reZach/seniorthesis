f5f51a09d7f60b61d97f304842528742ba586584
zach <zwenta@carthage.edu>
2015-03-21 23:02:16 -0500

    refactored and optimized code for improved ui

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,7 +9,7 @@ app.config([
                 views: {
                     "hub": {
                         templateUrl: "/views/partials/main.html" ,
-                        controller: "activityCtrl"
+                        controller: "activityCtrl2"
                     },
                     "accountHub": {
                         templateUrl: "/views/partials/userHub.html",
@@ -24,41 +24,314 @@ app.config([
 
 app.run(["$rootScope", function ($rootScope) {
 
+    // Run any code on startup if we wanted to
+    
+    /*
     (function () {
-        /*
-            Sets up cookies for our app:
-            
-
-            "balanceCookiesHasVisited" User has been to the app before
-            "balanceCookiesActivities" Activities user has made and/or kept track of
-            "balanceCookiesActivitiesTime" Time alloted for each activity
-
-        */
-
-        if (docCookies.getItem("balanceCookiesHasVisited") == null) {
-            
-            docCookies.removeItem("balanceCookiesHasVisited");
-            docCookies.removeItem("balanceCookiesActivities");
-            docCookies.removeItem("balanceCookiesActivitiesTime");
-
-            docCookies.setItem("balanceCookiesHasVisited", true, Infinity);
-        }
-        
+       
     }())
+    */
 
 }]);
 
-
-
-app.factory("userService", function() {
-    var o = {};
+// dataServiceFactory
+app.factory("activityService", function() {
     
-    o.ref = new Firebase("https://balanceapp.firebaseio.com");
+    var obj = {
+        
+        // Holds all data
+        data: {
+            activities: []
+        },
+        
+        // Sets cookie
+        setActivitiesCookie: function(activities){
+            docCookies.setItem("activities", JSON.stringify(activities), Infinity);
+        },
+        
+        // Initializes activities on page-load for the controller
+        initActivities: function() {
+        
+            var activities = obj.getActivities();
+            
+            angular.copy(activities, obj.data.activities);
+        },
+        
+        // Returns array of activities
+        getActivities: function() {
+        
+            if (docCookies.hasItem("activities")) {
+
+                var activities = JSON.parse(docCookies.getItem("activities"));
+
+                // Cast into an array if not already an array
+                if (activities.constructor != Array) {
+
+                    activities = [activities];
+                }
+
+                // Sort alphabetically by name
+                activities.sort(
+                    function (a, b) {
+                        if (a.name > b.name) { return 1; }
+                        if (b.name < a.name) { return -1; }
+                        return 0;
+                    });
+
+                return activities;
+            }
+
+            return [];
+        },
+        
+        // Adds new activity
+        addActivity: function(activityName){
+        
+            if (activityName === "undefined" || !activityName) {
+                throw "Cannot add activity with no name";
+            }            
+            
+            var activities;
+            
+            // If our cookie doesn't exist
+            if (!docCookies.hasItem("activities")) {
+
+                docCookies.setItem("activities", 
+                    [JSON.stringify({ 
+                        name: activityName, 
+                        active: false,
+                        selected: false,
+                        data: []
+                    })], 
+                    Infinity);
+                    
+                activities = obj.getActivities();               
+            } else {
+
+                activities = obj.getActivities();
+
+                for (var i = 0; i < activities.length; i++) {
+                    
+                    // Don't add activity if it already exists
+                    if (activities[i].name == activityName) {
+                        throw "Activity already exists";
+                    }
+                }
+
+                activities.push({ 
+                    name: activityName, 
+                    active: false,
+                    selected: false,
+                    data: []
+                });
+                
+                obj.setActivitiesCookie(activities);                              
+            }
+            
+            // Needed to update controllers properly
+            angular.copy(activities, obj.data.activities);
+        },
+        
+        // Selects an activity
+        selectActivity: function(activityName){
+        
+            if (activityName === "undefined" || !activityName) {
+                throw "Unknown error";
+            }
+            
+            var activities = obj.getActivities();
+            
+            for (var i = 0; i < activities.length; i++){
+            
+                if (activities[i].name == activityName){                
+                    activities[i].selected = true;
+                } else {
+                    activities[i].selected = false;
+                }
+            }
+            
+            obj.setActivitiesCookie(activities);
+            
+            // Needed to update controllers properly
+            angular.copy(activities, obj.data.activities);
+        },
+        
+        // Starts the currently selected activity
+        startSelectedActivity: function(){
+        
+            var selectedActivity;
+            var activities = obj.getActivities();
+            
+            for (var i = 0; i < activities.length; i++){
+            
+                // Find selected activity
+                if (activities[i].selected){
+                    
+                    selectedActivity = activities[i];
+                    activities[i].active = true;
+                } else {
+                
+                    activities[i].active = false;
+                }
+            }
+            
+            if (selectedActivity === "undefined" || !selectedActivity){
+                throw "Could not find selected activity";
+            }
+        
+        
+            /* Calculation logic */
+            
+            var dataBlock; // Holds reference to data block we will be modifying
+            var currentTime = Date.now();
+            var currentDate = new Date().toJSON();            
+            
+            if (selectedActivity.data.length == 0){ // This activity hasn't been started before
+            
+                // Add first record to this activity
+                selectedActivity.data.push({
+                    time: 0,
+                    date: currentDate,
+                    timestamp: currentTime
+                });
+                
+            } else {
+            
+                // We may need to add a new data block
+                // if the most recent data block doesn't
+                // belong to the current day
+                
+                
+                var activityDataDate;
+                currentDate = new Date(currentTime);
+                
+                // Look at the most recent data block                                
+                dataBlock = selectedActivity.data[selectedActivity.data.length - 1];                    
+                activityDataDate = new Date(dataBlock.date).setHours(0, 0, 0, 0);
+                
+                // If the current date is greater than the
+                // date of the data block, we need to create
+                // a new data block for this activity
+                if (currentDate.setHours(0, 0, 0, 0) > activityDataDate){
+                
+                    // Create new data block
+                    selectedActivity.data.push({
+                        time: 0,
+                        date: currentDate.toJSON(),
+                        timestamp: currentTime
+                    });
+                }                
+            }
+            
+            obj.setActivitiesCookie(activities);
+            
+            // Needed to update controllers properly
+            angular.copy(activities, obj.data.activities);
+        }, 
+        
+        // Updates time on all activities
+        recalculateActivityTimes: function(){
+        
+            var activities = obj.getActivities();
+            var currentTime = Date.now();
+            var currentDate = new Date(currentTime);
+            var activityDataDate;
+            
+            // Loop through all activities
+            for (var i = 0; i < activities.length; i++){
+            
+                for (var j = 0; j < activities[i].data.length; j++){
+                
+                    // Get date of the data block of the activity
+                    activityDataDate = new Date(activities[i].data[j].date).setHours(0, 0, 0, 0);
+                    
+                    // If the date of the data block and the current day are the same,
+                    // we don't have to make a new data block
+                    if (currentDate.setHours(0, 0, 0, 0) == activityDataDate){
+                    
+                        // Don't calculate anything if there is no need to
+                        if (activities[i].data[j].timestamp != 0){
+                        
+                            activities[i].data[j].time += (currentTime - activities[i].data[j].timestamp);
+                            
+                            // Our timestamp is re-assigned to the current time
+                            // if the activity is active, otherwise it is set to zero
+                            activities[i].data[j].timestamp (activities[i].active ? currentTime : 0);                            
+                        }
+                    } else if (currentDate.setHours(0, 0, 0, 0) > activityDataDate){
+                 
+                        // Since the current day has passed the last recorded day for the activity,
+                        // we need to calculate the time that hasn't been accounted for the previous
+                        // day before we calculate the time for the current day
+                        
+                        // Get the time of the end of the last recorded day
+                        var endOfActivityDataDate = new Date(activities[i].data[j].date).setHours(23, 59, 59, 999);
+                        var addToDataBlock = endOfActivityDataDate.getTime() - activityDataDate.getTime();
+                        
+                        activities[i].data[j].time += addToDataBlock;
+                        activities[i].data[j].timestamp = 0;
+                        
+                        // Make a new data block for the current day
+                        var newDataBlockDate = new Date().toJSON();
+                        var newDataBlockTime = currentTime - currentDate.setHours(0, 0, 0, 0);
+                        
+                        activities[i].data.push({
+                            date: newDataBlockDate,
+                            time: newDataBlockTime,
+                            timestamp: (activities[i].active ? currentTime : 0) // Set to currentTime if active, otherwise it is zero
+                        });
+                    }
+                }                
+            }
+            
+            obj.setActivitiesCookie(activities);
+            
+            // Needed to update controllers properly
+            angular.copy(activities, obj.data.activities);
+        },
+        
+        // Deletes an activity
+        deleteActivity: function(activityName){
+        
+            if (activityName === "undefined" || !activityName){
+                throw "Unknown error";
+            }
+            
+            var activities = obj.getActivities();
+            
+            for (var i = 0; i < activities.length; i++){
+            
+                if (activities[i].name == activityName){
+                    
+                    // Remove activity
+                    activities.splice(i, 1);
+                    break;
+                }
+            }
+            
+            obj.setActivitiesCookie(activities);
+            
+            // Needed to update controllers properly
+            angular.copy(activities, obj.data.activities);
+        }
+    };
     
-    return o;
+    return obj;
 });
 
+app.factory("graphService", ["activityService", function(activityService){
 
+    var obj = {
+        
+        // Holds all data
+        data: {
+            graph: {}
+        },
+    
+    };
+    
+    return obj;
+}]);
 
 app.service("dataService", function () {
 
@@ -405,6 +678,56 @@ app.factory("user", ["$http", "dataService", function($http, dataService){
     return o;
 }]);
 
+app.controller("activityCtrl2", ["$scope", "activityService", function ($scope, activityService) {
+
+    $scope.activities = activityService.data.activities;
+    activityService.initActivities();
+    
+    // Creates a new activity
+    $scope.newActivity = function(activityName){
+    
+        try
+        {
+            activityService.addActivity(activityName);            
+        } catch (e) {
+            alert(e);
+        }
+    };
+    
+    // Selects an activity
+    $scope.selectActivity = function(activityName){
+    
+        try
+        {
+            activityService.selectActivity(activityName);
+        } catch (e) {
+            alert(e);
+        }
+    };
+    
+    // Starts the selected activity
+    $scope.startSelectedActivity = function(){
+    
+        try
+        {
+            activityService.startSelectedActivity();
+        } catch (e) {
+            alert(e);
+        }
+    };
+    
+    // Deletes an activity
+    $scope.deleteActivity = function(activityName){
+    
+        try
+        {
+            activityService.deleteActivity(activityName);
+        } catch (e) {
+            alert(e);
+        }
+    };
+}]);
+
 app.controller("activityCtrl", ["$scope", "$interval", "dataService", function ($scope, $interval, dataService) {
     
     $scope.newActivity = "";
@@ -472,7 +795,7 @@ app.controller("activityCtrl", ["$scope", "$interval", "dataService", function (
 }]);
 
 
-app.controller("userCtrl", ["$scope", "userService", "user", function ($scope, userService, user) {
+app.controller("userCtrl", ["$scope", "user", function ($scope, user) {
 
     $scope.test = "hi";
     
